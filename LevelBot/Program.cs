9a1e5b9549788c7f3e73467562f3ce9b951a2405
EveryGameSPlay <repsbro@gmail.com>
2022-11-16 13:22:57 +0300

    Ini and basic functions

---
+++ LevelBot/Program.cs
@@ -0,0 +1,26 @@
+using Serilog;
+using Serilog.Events;
+using Serilog.Extensions.Logging;
+using Serilog.Sinks.SystemConsole.Themes;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var serilogLogger = CreateSerilog();
+var msLogger = new LoggerFactory().AddSerilog(serilogLogger).CreateLogger("main-logger");
+
+msLogger.LogWarning("Test");
+
+app.Run();
+
+
+Serilog.ILogger CreateSerilog()
+{
+    return new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+        .Enrich.FromLogContext()
+        .WriteTo.Console(theme: AnsiConsoleTheme.Sixteen)
+        .WriteTo.File($"{Environment.CurrentDirectory}/content/logs/logs.txt",rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+}
